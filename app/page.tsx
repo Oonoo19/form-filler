ea845ea3757cf492bf09a2f1d0afcd7cbc61a92f
Oyungerel Jargalsaikhan <oyungerel.j@ema.gov.mn>
2025-02-10 18:09:46 +0800

    fix: added module export

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 
-export default function Page() {
+export default function FormFillerPage() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-12 row-start-2 items-center sm:items-start">
@@ -18,3 +18,5 @@ export default function Page() {
     </div>
   );
 }
+
+module.exports = FormFillerPage;
