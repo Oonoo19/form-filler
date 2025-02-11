0bd73f1843d91db90a4bc67ccd9c46b15d938483
Oyungerel Jargalsaikhan <oyungerel.j@ema.gov.mn>
2025-02-11 11:58:29 +0800

    created dist folder

---
+++ dist/app/components/testPage.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+var TestPage = function () {
+    return _jsx("div", { children: "This is a test" });
+};
+export default TestPage;
---
+++ dist/app/page.js
@@ -0,0 +1,5 @@
+"use client";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+export default function FormFillerPage() {
+    return (_jsx("div", { className: "grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]", children: _jsxs("main", { className: "flex flex-col gap-12 row-start-2 items-center sm:items-start", children: [_jsx("h2", { className: "text-2xl", children: "Document builder" }), _jsx("div", { className: "flex gap-4 items-center flex-col sm:flex-row" })] }) }));
+}
